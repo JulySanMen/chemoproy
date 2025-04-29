6a5180edaa61fb6c5f4d2d2a6c3a28c942040a08
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-04-29 07:19:59 -0600

    Update server IP address in recibir.py

--- recibir.py
+++ recibir.py
@@ -1,6 +1,6 @@
 import socket
 
-server_ip = "192.168.1.96"  # Cambia esto por la IP real de B
+server_ip = "192.168.95.42"  # Cambia esto por la IP real de B
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((server_ip, 9999))
