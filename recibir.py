6060ba5957d884feb93afaead3857c02d1e11b34
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-04-29 18:07:03 -0600

    Update server IP address and port in recibir.py

--- recibir.py
+++ recibir.py
@@ -1,9 +1,9 @@
 import socket
 
-server_ip = "192.168.95.42"  # Cambia esto por la IP real de B
+server_ip = "192.168.95.76"  # Cambia esto por la IP real de B
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((server_ip, 9999))
+client_socket.connect((server_ip, 1290))
 
 with open("perr.jpg", "rb") as f:
     client_socket.sendfile(f)
